6952f1d6ba12e107439b15dedc24530a8eb2c0f4
Minsi Yang <yangminsi@gmail.com>
2020-05-07 15:33:37 +0100

    Added cohort input for input_student method

--- directory.rb
+++ directory.rb
@@ -1,17 +1,25 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students."
+  puts "Please enter the name of cohort."
+  puts "To finish, just hit return three times"
   # create an empty array
   students = []
-  # get the first name
+
   name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november, hobbies: :crafts, country_of_birth: :galaxy}
+  cohort = gets.chomp
+
+  while !name.empty? || !cohort.empty? do
+    month = ["january", "february", "march", "april", "may", "june", "july",
+      "august", "september", "october", "november", "december"]
+
+    name = "TBC" if name.empty?
+    cohort = "november" if cohort.empty? || !month.include?(cohort)
+#    add the student hash to the array
+    students << {name: name, cohort: cohort.to_sym, hobbies: :crafts, country_of_birth: :galaxy}
     puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
+    cohort = gets.chomp
   end
   # return the array of input_students
   students
