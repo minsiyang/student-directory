525c72fa556456eb2fbcce5f8b3b083b7f8e88be
Minsi Yang <yangminsi@gmail.com>
2020-05-09 11:20:39 +0100

    Added add_students method

--- step14.rb
+++ step14.rb
@@ -1,4 +1,6 @@
 @students = [] # an instance variable, accessible to all methods
+@name = nil
+@cohort = :november
 
 def print_menu
   puts "1. Input the students"
@@ -36,14 +38,39 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first name
-  name = STDIN.gets.chomp
+  @name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
-  while !name.empty? do
-    @students << {name: name, cohort: :november}
+  while !@name.empty? do
+    add_students
     puts "Now we have #{@students.count} students"
     # get another name from the user
-    name = STDIN.gets.chomp
+    @name = STDIN.gets.chomp
   end
+end
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+  @name, @cohort = line.chomp.split(",")
+    add_students
+  end
+  file.close
+end
+
+def add_students # add the hash to @students array
+  @students << {name: @name, cohort: @cohort.to_sym}
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 def show_students
@@ -67,33 +94,12 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
-def save_students
-  # open the file for writing
-  file = File.open("students.csv", "w")
-
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
-end
-
-def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close
-end
-
 def try_load_students
   filename = ARGV.first # first argument from the command line
   return if filename.nil? # get out of the method if it isn't given
   if File.exist?(filename)
     load_students(filename)
-     puts "Loaded #{@students.count} from #{filename}"
+    puts "Loaded #{@students.count} from #{filename}"
   else
     puts "Sorry, #{filename} doesn't exist."
     exit
