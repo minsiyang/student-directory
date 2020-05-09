7a175d38ed3c58f9cc92cc597e1f216cba251d4a
Minsi Yang <yangminsi@gmail.com>
2020-05-09 18:19:19 +0100

    updated print_menu method

--- step14.rb
+++ step14.rb
@@ -5,8 +5,8 @@
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
+  puts "3. Save the list to a file"
+  puts "4. Load the list from a file"
   puts "9. Exit"
 end
 
